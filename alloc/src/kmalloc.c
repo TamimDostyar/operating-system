449c6b1af697c260bc3fdbac217c18b6d9a6c53b
Tamim Dostyar <a.tamimdostyar@gmail.com>
2026-04-19 21:55:54 -0500

    memory allocation help fixed

--- alloc/src/kmalloc.c
+++ alloc/src/kmalloc.c
@@ -6,6 +6,15 @@ void *kmalloc(size_t size){
     
     while (cur){
         if (cur->isFree && cur->size >= size){
+            // for larger cases we need to still leave size
+            if (cur->size >= size + sizeof(block_header_t) + 1){
+                block_header_t *new_block = (block_header_t*)((char*)(cur + 1) + size);
+                new_block->size  = cur->size - size - sizeof(block_header_t);
+                new_block->isFree = 1;
+                new_block->next  = cur->next;
+                cur->size = size;
+                cur->next = new_block;
+            }
             cur->isFree = 0;
             return (void *) (cur +1);   
         }
